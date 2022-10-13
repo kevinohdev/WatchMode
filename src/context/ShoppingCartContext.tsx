e66de5d24fe6b39b31b98d420a7604f63eaa8cc4
macnine <kkoh545@pm.me>
2022-10-13 10:48:33 -0700

    renamed variable

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -51,11 +51,11 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   }
 
   const decreaseQty = (id: number) => {
-    setCartItems(currItems => {
-      if (currItems.find(item => item.id === id)?.quantity === 1) {
-        return currItems.filter(item => item.id !== id)
+    setCartItems(currentItems => {
+      if (currentItems.find(item => item.id === id)?.quantity === 1) {
+        return currentItems.filter(item => item.id !== id)
       } else {
-        return currItems.map(item => {
+        return currentItems.map(item => {
           if (item.id === id) {
             return { ...item, quantity: item.quantity - 1 }
           } else {
