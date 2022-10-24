3e0d1bb2a2ed87f4ded95e3b55986580b6f1a73b
macnine <kkoh545@pm.me>
2022-10-23 18:36:15 -0700

    updated pics

--- src/styles/StoreStyles.ts
+++ src/styles/StoreStyles.ts
@@ -4,15 +4,15 @@ const StoreStyles = styled.div`
   margin: 0 auto;
   max-width: 1200px;
 
-
-
 .grid {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-template-rows: repeat(auto-fill, minmax(400px, 1fr));
+  grid-template-rows: repeat(auto-fill, minmax(300px, 1fr));
   grid-gap: 2rem .5rem;
   justify-content: center;
   padding: 1rem;
-}`;
+}
+
+`;
 
 export default StoreStyles;
